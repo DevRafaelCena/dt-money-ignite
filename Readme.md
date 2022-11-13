4f4a1e31b6e80879d5958df9d65b291a75b8c54f
Rafael Cena <rafael.cena@hotmail.com>
2022-11-13 08:48:33 -0300

    modal

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Dt money
+
+Projeto para controle financeiro pessoal. Desenvolvimento durante a trilha ignite Reatcjs
+
+## Stack utilizada
+
+**Front-end:** Reactjs
+
+**Libs:**
+
+- Radix = https://www.radix-ui.com/
+- phosphor-react = https://phosphoricons.com/
+- styled-components = https://styled-components.com/
+- vite = https://vitejs.dev/
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="" />
+        <img src={logoImg} alt="" />        
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
