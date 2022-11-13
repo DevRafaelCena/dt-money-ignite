45e8340cce78eacbb4ff8307eee4845f3d9a1a41
Rafael Cena <rafael.cena@hotmail.com>
2022-11-13 08:05:50 -0300

    search

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { SearchForm } from "./components/SearchForm";
+
 
 export function Transactions() {
   return (
@@ -9,6 +11,7 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
